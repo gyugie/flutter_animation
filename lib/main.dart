3e985e241cb004dd2f23c8609dfe1a36172f22da
Mugi <mugypleci@gmail.com>
2019-07-25 11:10:46 +0700

    Animate the properties of a container

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,72 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    home: Page1(),
-    ));
+void main() => runApp(AnimatedContainerApp());
+
+class AnimatedContainerApp extends StatefulWidget {
+  @override
+  _AnimatedContainerAppState createState() => _AnimatedContainerAppState();
 }
 
-class Page1 extends StatelessWidget {
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  // Define the various properties with default values. Update these properties
+  // when the user taps a FloatingActionButton.
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Go!'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AnimatedContainer Demo'),
+        ),
+        body: Center(
+          child: AnimatedContainer(
+            // Use the properties stored in the State class.
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadius,
+            ),
+            // Define how long the animation should take.
+            duration: Duration(seconds: 1),
+            // Provide an optional curve to make the animation feel smoother.
+            curve: Curves.fastOutSlowIn,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          // When the user taps the button
           onPressed: () {
-            Navigator.of(context).push(_createRoute());
+            // Use setState to rebuild the widget with new values.
+            setState(() {
+              // Create a random number generator.
+              final random = Random();
+
+              // Generate a random width and height.
+              _width = random.nextInt(300).toDouble();
+              _height = random.nextInt(300).toDouble();
+
+              // Generate a random color.
+              _color = Color.fromRGBO(
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256),
+                1,
+              );
+
+              // Generate a random border radius.
+              _borderRadius =
+                  BorderRadius.circular(random.nextInt(100).toDouble());
+            });
           },
         ),
       ),
     );
   }
 }
-
-Route _createRoute(){
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child){
-      var begin = Offset(0.0, 1.0);
-      var end   = Offset.zero;
-      var curve = Curves.ease;
-
-      var tween = Tween(begin: begin, end: end);
-      var curvedAnimation = CurvedAnimation(
-      parent: animation,
-      curve: curve,
-      );
-
-         return SlideTransition(
-            position: tween.animate(curvedAnimation),
-            child: child,
-          );
-    },
-  );
-}
-
-class Page2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Page 2'),
-      ),
-    );
-  }
-}
-
