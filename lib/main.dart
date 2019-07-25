f41a21b8cd6fc357da8cf7216426333a3a8f99c9
Mugi <mugypleci@gmail.com>
2019-07-25 17:29:51 +0700

    Design with sidebar and config for release APK

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,107 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(FadeOut());
 
-class FadeOut extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Air Port - Crazy Wave';
+
   @override
   Widget build(BuildContext context) {
-    final TitleApps = 'Opacity Demo';
     return MaterialApp(
-      title: TitleApps,
-      home: MyHomePage(title: TitleApps),
+      title: appTitle,
+      home: MyHome(title: appTitle),
     );
   }
 }
 
-//the statefull widget job is to take data and create s state class
-//in this case, the widget takes a title and creates a _MyhomePageState
-class MyHomePage extends StatefulWidget {
+class MyHome extends StatelessWidget {
   final String title;
-
-  MyHomePage({Key key, this.title}) : super (key : key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-//the State class is responsible for two things: holdings some data you can
-//update and buiolding the UI using that data
-class _MyHomePageState extends State<MyHomePage> {
-  bool _visible = true;
+  
+  MyHome({Key key, this.title}) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
-   // The green box goes here with some other Widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-        child: AnimatedOpacity(
-          //If the widget is visible, animate to 0.0 (invisible)
-          //If the widget is hiddenm animate to 1.0 (fully visible)
-          opacity: _visible ? 0.0 : 1.0,
-          duration: Duration(milliseconds: 1000),
-          //the green box must be a child of the AnimatedOpacity widget
-          child: Container(
-            width: 200.0,
-            height: 200.0,
-            color: Colors.green,
-          ),
+        child: Text('Body Page'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          //importan : remove any padding from the ListView
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text("gyugie"),
+              accountEmail: new Text("mugypleci@gmail.com"),
+              onDetailsPressed: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.flight_takeoff),
+              title: Text('Arrival'),
+              onTap:(){
+                //Update the state of the app
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => ArrivalPage() ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.flight_land),
+              title: Text('Depature'),
+              onTap: () {
+                //Update the state of the app
+                //...
+                 Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => DepaturePage() ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),//add Drawer here in the next step,
+    );
+  }
+}
+
+class ArrivalPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Arrival Page'),
+        ),
+      body: Center(
+         child: Column( // Replace with a Row for horizontal icon + text
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.flight_land, size: 50),
+            ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Call state. This tells Flutter to rebuild the
-          //UI with the changes
-          setState(() {
-            _visible = !_visible;
-          });
-        },
-        tooltip: 'Toggle Opacity',
-        child: Icon(Icons.flip),
+    );
+  }
+}
+
+class DepaturePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Depature Page'),
+        ),
+      body: Center(
+         child: Column( // Replace with a Row for horizontal icon + text
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.flight_takeoff, size: 50),
+            ],
+        ),
       ),
     );
   }
