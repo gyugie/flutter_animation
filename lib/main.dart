038163f9c126657524bd7e3725a02231278fef45
Mugi <mugypleci@gmail.com>
2019-08-07 16:21:29 +0700

    [design] build form with validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(TabBarDemo());
+void main() => runApp(MyApp());
 
-class TabBarDemo extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    final appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+
+//create a form widget
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState(){
+    return MyCustomFormState();
+  }
+}
+
+//create a corresponding State class
+// this class holds data related to the form
+class MyCustomFormState extends State<MyCustomForm>{
+  //create global key that uniquely identifies the form widget
+  //and validation of the form
+
+  final _formKey = GlobalKey<FormState>();
+
   @override 
   Widget build(BuildContext context){
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs:[
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-                Tab(icon: Icon(Icons.directions_walk)),
-              ],
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value){
+              if(value.isEmpty){
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical:16.0),
+            child: RaisedButton(
+              onPressed: (){
+                //validate return true if the form is valid or false
+                //otherwise
+
+                if(_formKey.currentState.validate()){
+                  //if the form valid show a snackbar
+                  print(_formKey);
+                   Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
             ),
-            title: Text('Tabs Demo'),
           ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.directions_railway),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-              Icon(Icons.directions_walk),
-            ],
-          ),
-        ),
+        ],
       ),
     );
   }
