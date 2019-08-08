33eea254d6070ae83217e8f8a64bb0e56b04f5ef
Mugi <mugypleci@gmail.com>
2019-08-08 10:31:42 +0700

    [images] Fade in images with a placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,29 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() => runApp(MyApp());
 
-//MyApp is a StatefulWidget This allows updating the state of the
-//widget when an item is removed
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
-  @override
- @override
-  MyAppState createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  final items = List<String>.generate(10, (i) => "Item ${i + 1}");
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   final title = 'Dismis Items';
-
+    var title = 'Web Images';
+    
     return MaterialApp(
       title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index){
-          final item = items[index];
-
-          return Dismissible(
-            //Each Dissmisible must contain a key, keys allow flutter to
-            //uniquely identify widgets
-            key: Key(item),
-            //provide a function that tells the app
-            //what to do after an item has been swiped away
-            onDismissed: (direction){
-              //remove the item from the data source
-              setState(() {
-                items.removeAt(index);
-              });
-
-              //Then show a sanckbar
-              Scaffold.of(context)
-              .showSnackBar(SnackBar(content: Text("$item dismissed")));
-            },
-
-            //show a red background as the item is swipped away
-            background: Container(color: Colors.redAccent),
-            child: ListTile(title: Text('$item')),
-            );
-          },
+        body: Stack(
+          children: <Widget>[
+            Center(child: CircularProgressIndicator()),
+            Center(
+              child:FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: 'https://fotos00.formulamoto.es/2016/05/19/646x260/ktmenduro2017.jpg',
+              ),
+            ),
+          ],
         ),
       ),
     );
